12dc4aaf93a4364030366766d5f01baa16ac66c3
abhishek56230-afk <abhishek56230@gmail.com>
2026-03-18 21:53:06 -0300

    Refactor app.run to use environment variable for port

--- app.py
+++ app.py
@@ -643,6 +643,5 @@ def watchlist_verdict(symbol):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False, host="0.0.0.0", port=port, threaded=True)
-   
+   app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), threaded=True)
 
